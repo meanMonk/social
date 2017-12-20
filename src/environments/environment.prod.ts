deddf7430ed9fb97de86e642c0c178250c60312b
sahil kashetwar <sahilkashetwar24@gmail.com>
2017-12-20 11:22:18 +0530

    creating the build for productions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    firebase: {
+      apiKey: 'AIzaSyDle6JN3WT5JbPCrbfMziuwopP-BdRS5zk',
+      authDomain: 'social-45178.firebaseapp.com',
+      databaseURL: 'https://social-45178.firebaseio.com',
+      projectId: 'social-45178',
+      storageBucket: 'social-45178.appspot.com',
+      messagingSenderId: '71163330716'
+    }
+  }
 };
